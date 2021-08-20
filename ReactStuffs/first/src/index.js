a805a4c0e728ddf355c2f4adaccdf95c197c720a
razzaksr <razzaksr@gmail.com>
2021-08-20 20:37:42 +0530

    first react app

---
+++ ReactStuffs/first/src/index.js
@@ -0,0 +1,10 @@
+import rec from 'react';
+import recdom from 'react-dom';
+
+recdom.render(
+  <>
+    <h1 contentEditable="true">Welcome to Dlithe Front end development</h1>
+    <p>Javascript offers React library</p>
+  </>,
+  document.getElementById('root')
+)
